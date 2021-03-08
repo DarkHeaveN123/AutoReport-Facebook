92e102c6f0e77f45b774827e32bdd10b009ac6b3
DARKHEAVEN <79021904+DarkHeaveN123@users.noreply.github.com>
2021-03-08 19:35:53 +0530

    Create autoreport.py

---
+++ DARKHEAVEN/ACCESS Code/Automation/autoreport.py
@@ -0,0 +1,80 @@
+#!/bin/bash
+clear
+figlet "Auto Report"
+echo  "Author   : [DARKHEAVEN]"
+echo  "TEAM     : [DARKHOLE]"
+echo  "INSTAGRAM: [dark_h_e_a_v_e_n]"
+echo  "GITHUB   : [DarkHeaveN123]"
+echo -e "\e[1;41;36mFacebook Auto Report Tool\e[0m"
+sleep 5
+echo -e "\e[1;;37m[Loading Datas..]\e[0m"
+sleep 4
+echo -e "\e[1;;33m[Reporting Account Searching..]\e[0m"
+sleep 5
+echo -e "\e[1;;37m[Total Available Reporting Accounts 5682]\e[0m"
+sleep 5
+echo -e "\e[1;102;36mEnter Username:\e[0m"
+read username
+echo -e "\e[1;;34m[Checking Database...]\e[0m"
+sleep 9
+echo -e "\e[1;;36m[Connect Sever..]\e[0m"
+sleep 10
+echo -e "\e[1;;31m[Darkheaven Sever Connect]\e[0m"
+sleep 6
+echo -e "\e[1;102;37mEnter Password:\e[0m"
+read password
+
+if [[ ( $username == "Darkheaven" && $password == "Dark" ) ]]; then
+echo -e "\e[1;40;32m{Tool Actived}\e[0m"
+else
+echo -e "\e[1;;31m{Wrong Entry}\e[0m"
+fi
+sleep 5
+echo -e "\e[1;;34m[TOR Connecting..]\e[0m"
+sleep 5
+echo -e "\e[1;;37m[TOR Sucessfully Connected ]\e[0m"
+sleep 4
+echo -e "\e[1;41;36mEnter Your Target Id:\e[0m"
+read Your Target Id
+sleep 6
+clear
+figlet "Reporting Starting"
+echo  "[                    ] 0% "
+sleep 5
+echo  "[=====               ] 25%"
+sleep 3
+echo  "[==========          ] 50%"
+sleep 6
+echo  "[===============     ] 75%"
+sleep 5
+echo  "[====================] 100%"
+sleep 4
+figlet "Report Account  Starting"
+echo  "[                    ] 0% "
+sleep 5
+echo  "[=====               ] 25%"
+sleep 3
+echo  "[==========          ] 50%"
+sleep 6
+echo  "[===============     ] 75%"
+sleep 4
+echo  "[====================] 100%"
+sleep 5
+echo -e "\e[1;;34m[Darkheaven Domain Active]\e[0m"
+sleep 6
+figlet "Final Report  Starting"
+echo  "[                    ] 0% "
+sleep 5
+echo  "[=====               ] 25%"
+sleep 3
+echo  "[==========          ] 50%"
+sleep 6
+echo  "[===============     ] 75%"
+sleep 9
+echo  "[====================] 100%"
+sleep 7
+echo -e "\e[1;102;37mPlease Wait Check Final Reslut..\e[0m"
+sleep 10
+echo -e "\e[1;;34m[Loading Result..]\e[0m"
+sleep 13
+figlet "Report Failure"
